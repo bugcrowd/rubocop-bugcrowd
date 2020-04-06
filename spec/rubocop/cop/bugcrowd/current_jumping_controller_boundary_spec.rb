36992fc69b4b6bb8b44562ec4ffc33804606c42b
Max Schwenk <maschwenk@gmail.com>
2020-04-06 14:12:44 -0400

    Add CurrentJumpingControllerBoundary cop (#7)

---
+++ spec/rubocop/cop/bugcrowd/current_jumping_controller_boundary_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Bugcrowd::CurrentJumpingControllerBoundary do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offense when using Current' do
+    expect_offense(<<~RUBY)
+      Current.baz.bar
+      ^^^^^^^^^^^ Curent should not be used outside of controllers. Pass it along to other systems that need it
+    RUBY
+  end
+
+  it 'does not register an offense for unrelated const' do
+    expect_no_offenses(<<~RUBY)
+      Flurrent.baz.bar
+    RUBY
+  end
+end
