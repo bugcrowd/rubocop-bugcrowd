244f3e0d5f2f96a4bf7a0542ecbb27ee4e23480f
Max Schwenk <maschwenk@gmail.com>
2021-01-12 15:08:38 -0500

    Add documentation link to `Bugcrowd/DangerousTransaction` (#22)

--- spec/rubocop/cop/bugcrowd/dangerous_transaction_spec.rb
+++ spec/rubocop/cop/bugcrowd/dangerous_transaction_spec.rb
@@ -8,21 +8,21 @@ RSpec.describe RuboCop::Cop::Bugcrowd::DangerousTransaction do
   it 'registers an offense when using class level method' do
     expect_offense(<<~RUBY)
       Model.transaction { doing_a_thing }
-      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
     RUBY
   end
 
   it 'registers an offense when using class level method' do
     expect_offense(<<~RUBY)
       Model.transaction { |d| d.doing_a_thing }
-      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
     RUBY
   end
 
   it 'registers an offense when using class level method' do
     expect_offense(<<~RUBY)
       Model.transaction do |d|
-      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
         d.doing_a_thing
       end
     RUBY
@@ -31,7 +31,7 @@ RSpec.describe RuboCop::Cop::Bugcrowd::DangerousTransaction do
   it 'registers an offense when using class level method' do
     expect_offense(<<~RUBY)
       Model.transaction(joinable: true, requires_new: true) do |d|
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
         d.doing_a_thing
       end
     RUBY
@@ -40,14 +40,14 @@ RSpec.describe RuboCop::Cop::Bugcrowd::DangerousTransaction do
   it 'registers an offense when using base class level method' do
     expect_offense(<<~RUBY)
       ActiveRecord::Base.transaction { doing_a_thing }
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
     RUBY
   end
 
   it 'registers an offense when using base class level method' do
     expect_offense(<<~RUBY)
       ActiveRecord::Base.transaction { doing_a_thing }
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods.
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use ProperTransaction.start instead of ActiveRecord's base, class, or instance-level transaction methods. See https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails for more background
     RUBY
   end
 
