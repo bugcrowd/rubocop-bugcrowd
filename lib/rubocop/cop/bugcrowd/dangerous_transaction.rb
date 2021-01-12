abdcc992f6a1432ae9c2667aba792a1c9bf9b560
Max Schwenk <maschwenk@gmail.com>
2021-01-12 14:41:00 -0500

    Lol if someone has a better way....

--- lib/rubocop/cop/bugcrowd/dangerous_transaction.rb
+++ lib/rubocop/cop/bugcrowd/dangerous_transaction.rb
@@ -7,7 +7,8 @@ module RuboCop
       class DangerousTransaction < Cop
         MSG = "Use ProperTransaction.start instead of ActiveRecord's base, " \
               'class, or instance-level transaction methods. See ' \
-              'https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails' \
+              'https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/' \
+              'Learnings+-+Nested+Transactions+in+Rails' \
               ' for more background'
 
         def_node_matcher :consts_that_transaction_may_be_called_on, <<-PATTERN
