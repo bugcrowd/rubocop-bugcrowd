fd310986e548b7868faaf769a3dfaa78780f9589
Max Schwenk <maschwenk@gmail.com>
2021-01-12 14:34:36 -0500

    Add link to confluence

--- lib/rubocop/cop/bugcrowd/dangerous_transaction.rb
+++ lib/rubocop/cop/bugcrowd/dangerous_transaction.rb
@@ -6,7 +6,9 @@ module RuboCop
     module Bugcrowd
       class DangerousTransaction < Cop
         MSG = "Use ProperTransaction.start instead of ActiveRecord's base, " \
-              'class, or instance-level transaction methods.'
+              'class, or instance-level transaction methods. See ' \
+              'https://bugcrowd.atlassian.net/wiki/spaces/DEV/blog/2020/03/24/1126006819/Learnings+-+Nested+Transactions+in+Rails' \
+              ' for more background'
 
         def_node_matcher :consts_that_transaction_may_be_called_on, <<-PATTERN
           {(send (const nil? _) :transaction ...) (send (const (const nil? :ActiveRecord) :Base) :transaction ...)}
