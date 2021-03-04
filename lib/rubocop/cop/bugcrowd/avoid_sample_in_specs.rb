a9e671d26bd77a21ed8a42bc47d9ff9cf20e8d20
Max Schwenk <maschwenk@gmail.com>
2021-03-04 13:05:49 -0500

    Bugcrowd/AvoidSampleInSpecs (#25)

---
+++ lib/rubocop/cop/bugcrowd/avoid_sample_in_specs.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# TODO: when finished, run `rake generate_cops_documentation` to update the docs
+module RuboCop
+  module Cop
+    module Bugcrowd
+      #
+      #   # bad
+      #   thing = list_of_things.sample
+      #   expect(thing).to have_some_property
+      #
+      #   # good
+      #   list_of_things.each do |thing|
+      #     expect(thing).to have_some_property
+      #   end
+      #
+      #   # good
+      #   expect(thing).to have_some_property
+      #   expect(thing1).to have_some_property
+      #
+      class AvoidSampleInSpecs < Cop
+        MSG = 'Avoid using sample in spec as it can cause non-deterministic behavior'
+
+        def_node_matcher :sample?, <<~PATTERN
+          (send _ :sample ...)
+        PATTERN
+
+        def on_send(node)
+          return unless sample?(node)
+
+          add_offense(node)
+        end
+      end
+    end
+  end
+end
