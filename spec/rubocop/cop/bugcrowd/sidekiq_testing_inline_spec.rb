bf6292a8a4792327c0d863de6ed9c7d982dea1be
Federico Tagliabue <federico.tagliabue@bugcrowd.com>
2021-03-04 14:47:27 +1100

    discourage usage of Sidekiq::Testing.inline! (#24)
    * discourage usage of Sidekiq::Testing.inline!
    
    * spec with more real life code example

---
+++ spec/rubocop/cop/bugcrowd/sidekiq_testing_inline_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Bugcrowd::SidekiqTestingInline do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offence when calling Sidekiq::Testing::Inline! with a block' do
+    expect_offense(<<~RUBY)
+      Sidekiq::Testing.inline! do
+      ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer to drain the worker, then calling `Sidekiq::Testing.inline!`
+        subject
+      end
+    RUBY
+  end
+
+  it 'registers an offence when calling Sidekiq::Testing::Inline! with an inline block' do
+    expect_offense(<<~RUBY)
+      Sidekiq::Testing.inline! { subject }
+      ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer to drain the worker, then calling `Sidekiq::Testing.inline!`
+    RUBY
+  end
+
+  it 'does not register an offense when calling another method' do
+    expect_no_offenses(<<~RUBY)
+      Sidekiq::Testing.offline
+    RUBY
+  end
+end
