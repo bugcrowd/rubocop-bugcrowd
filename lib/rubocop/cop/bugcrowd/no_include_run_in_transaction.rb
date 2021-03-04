c8b39c5b9174e162970980c5a45844a1c4ac9743
Charlotte Manetta <charlotte.manetta@bugcrowd.com>
2021-03-04 09:13:28 -0800

    Add NoIncludeRunInTransaction cop (#21)
    Co-authored-by: Adam David <adamrdavid@gmail.com>
    Co-authored-by: Charlotte Manetta <charlotte.manetta@bugcrowd.com>

---
+++ lib/rubocop/cop/bugcrowd/no_include_run_in_transaction.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Bugcrowd
+      #   Commands often need to publish events, so wrapping the whole thing in a transaction
+      #   means that the event is published within the transaction. We prefer explicit
+      #   transactions defined only where they need to go. Many event handlers run
+      #   asynchronously which can cause them to run even when the action has been rolled back.
+      #
+      #   # bad
+      #   class NewCommand
+      #     include Interactor
+      #     include RunInTransaction
+      #     ...
+      #   end
+      #
+      #   # good
+      #   class NewCommand
+      #     include Interactor
+      #
+      #     def call
+      #       ActiveRecord::Base.transaction do
+      #         ...
+      #       end
+      #     end
+      #   end
+      #
+
+      class NoIncludeRunInTransaction < Cop
+        MSG = 'Prefer explicit transactions over wrapping entire command or organizer ' \
+              'see: https://bugcrowd.atlassian.net/wiki/spaces/DEV/pages/589856783/How+to+use+activerecord+transactions'
+
+        def_node_matcher :including_run_in_transaction?, <<~PATTERN
+          (send nil? :include (const nil? :RunInTransaction))
+        PATTERN
+
+        def on_send(node)
+          add_offense(node) if including_run_in_transaction?(node)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/bugcrowd/no_include_run_in_transaction_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Bugcrowd::NoIncludeRunInTransaction do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it do
+    expect_offense(<<~RUBY)
+      class NewCommand
+        include Interactor
+        include RunInTransaction
+        ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer explicit transactions over wrapping entire command or organizer see: https://bugcrowd.atlassian.net/wiki/spaces/DEV/pages/589856783/How+to+use+activerecord+transactions
+      end
+    RUBY
+  end
+
+  it do
+    expect_offense(<<~RUBY)
+      class Commands::NewCommand
+        include RunInTransaction
+        ^^^^^^^^^^^^^^^^^^^^^^^^ Prefer explicit transactions over wrapping entire command or organizer see: https://bugcrowd.atlassian.net/wiki/spaces/DEV/pages/589856783/How+to+use+activerecord+transactions
+        include Interactor
+      end
+    RUBY
+  end
+
+  it 'does not register an offense when using explicit transactions' do
+    expect_no_offenses(<<~RUBY)
+      class NewCommand
+        include Interactor
+
+        def call
+          ActiveRecord::Base.transaction do
+            new_record.save!
+          end
+        end
+      end
+    RUBY
+  end
+end
