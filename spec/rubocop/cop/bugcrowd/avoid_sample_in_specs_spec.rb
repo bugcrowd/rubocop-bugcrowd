b12dcd6a19c4d6f84ef7b953fb838c628c0f100f
Max Schwenk <maschwenk@gmail.com>
2021-03-04 12:58:32 -0500

    Red test from @adam's suggestion

--- spec/rubocop/cop/bugcrowd/avoid_sample_in_specs_spec.rb
+++ spec/rubocop/cop/bugcrowd/avoid_sample_in_specs_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe RuboCop::Cop::Bugcrowd::AvoidSampleInSpecs do
     RUBY
   end
 
+  it 'registers an offense when using sample with single arg' do
+    expect_offense(<<~RUBY)
+      blah.zah.sample(3)
+      ^^^^^^^^^^^^^^^^^^ Avoid using sample in spec as it can cause non-deterministic behavior
+    RUBY
+  end
+
+  it 'registers an offense when using sample with random' do
+    expect_offense(<<~RUBY)
+      blah.zah.sample(3, random: 1)
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid using sample in spec as it can cause non-deterministic behavior
+    RUBY
+  end
+
   it 'does not register an offense when using a different method' do
     expect_no_offenses(<<~RUBY)
       blah.zah.not_sample
