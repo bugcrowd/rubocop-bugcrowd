983379a84fa3e884e25841d3d9c3c0368a6e72e3
Max Schwenk <maschwenk@gmail.com>
2021-03-04 12:49:27 -0500

    Move PR decsription into cop{

--- lib/rubocop/cop/bugcrowd/prefer_sensible_string_enum.rb
+++ lib/rubocop/cop/bugcrowd/prefer_sensible_string_enum.rb
@@ -3,6 +3,12 @@
 module RuboCop
   module Cop
     module Bugcrowd
+      #  SensibleStringEnum has a number of advantages over the built in Rails enum.
+      #  1. Rails string enums are not supported very well. They are really meant for
+      #     traditional integer enums -- which I've never actually seen used.
+      #  2. Rails enums throw runtime exceptions when they are SET instead of at validation
+      #  3. Rails enums add a ton of methods to the class they are added to, many of which are
+      #     not very helpful
       #
       #   # bad
       #   enum source: { web: 'web', csv: 'csv' }
