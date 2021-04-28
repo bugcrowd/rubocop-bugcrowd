028036dba1c6fb278e67614c4b887b0fb9a2c3c7
Max Schwenk <maschwenk@gmail.com>
2021-04-28 13:42:34 -0700

    Add good/bad examples

--- lib/rubocop/cop/bugcrowd/prefer_request_yielding_response_pattern.rb
+++ lib/rubocop/cop/bugcrowd/prefer_request_yielding_response_pattern.rb
@@ -4,6 +4,52 @@ module RuboCop
   module Cop
     module Bugcrowd
       class PreferRequestYieldingResponsePattern < Cop
+        #
+        # @example
+        #
+        # # bad
+        # ```
+        # before { post user_path }
+        # it 'something happens' do
+        #   expect(response).to have_http_status :ok
+        # end
+        # ```
+        #
+        # # bad
+        # This pattern is not easily parseable, but is no better than
+        # the above pattern because it just gives the before a different name
+        # ```
+        # before { request }
+        # let(:request) { get user_path }
+        # it 'something happens' do
+        #   expect(response).to have_http_status :ok
+        # end
+        # ```
+        #
+        # # good
+        # Seperate the subject of the test from the test setup
+        # Use late binding to allow for more predictable behavior
+        # ```
+        #  subject(:request_yielding_response) do
+        #    http_request
+        #    response
+        #  end
+        #  let(:http_request) { get user_path(name: 'dilbert') }
+        #  it { is_expected.to be_ok }
+        #  it 'blah' do
+        #    expect { request_yielding_response }.to change(User, :count).by(1)
+        #  end
+        # ```
+        #
+        # # good
+        # Directly calling the request method is fine too!
+        # ```
+        #  it 'blah' do
+        #    get user_path(name: 'dilbert')
+        #  end
+        # ```
+        #
+
         MSG = 'Prefer request_yielding_response pattern. See https://gist.github.com/maschwenk/6eaf0a3cbf0e6f1432b923cbca7e34d1'
 
         def_node_matcher :begin_before_block?, <<~PATTERN
