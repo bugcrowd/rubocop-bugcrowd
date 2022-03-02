7ab7688ed0631c2df213de128957a4596a01f2a0
Federico Tagliabue <federico.tagliabue@bugcrowd.com>
2022-03-03 08:38:56 +1100

    update specs

--- spec/rubocop/cop/bugcrowd/sleepy_specs_spec.rb
+++ spec/rubocop/cop/bugcrowd/sleepy_specs_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe RuboCop::Cop::Bugcrowd::SleepySpecs do
   xit 'registers an offense when using `#bad_method`' do
     expect_offense(<<~RUBY)
       sleep(1)
-      ^^^^^^^^ 🚨  Do not use sleep, use page.driver.wait_for_network_idle instead 🚨
+      ^^^^^^^^ 🚨  Do not use sleep, wait_for_network_idle or wait instead 🚨
     RUBY
   end
 
