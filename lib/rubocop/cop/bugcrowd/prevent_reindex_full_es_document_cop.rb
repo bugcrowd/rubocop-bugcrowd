a75a37e9feb67eff12c7d7ca46b88cc11d007417
Bugcrowd <adarsha.ks@bugcrowd.com>
2024-06-25 16:08:54 +0530

    Add PreventReindexFullESDocumentCop

---
+++ lib/rubocop/cop/bugcrowd/prevent_reindex_full_es_document_cop.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Bugcrowd
+      class PreventReindexFullESDocumentCop < Cop
+        #
+        # @example
+        #
+        # # bad
+        # ```
+        # Reindexing a full ES document would reindex all the resource ids
+        # ```
+        # ValisCommands::ReindexDocument.call(document_type: 'SubmissionDocument')
+        #
+        # # good
+        # Reindex only a specific resource ID instead
+        # ```
+        # ValisReindexWorker.new.perform([submission.id], Submission.to_s)
+        # ```
+        #
+
+        MSG = 'Avoid reindexing the full Elasticsearch document. Consider reindexing only specific resource ids.'
+
+        def_node_matcher :valis_reindex_document?, <<-PATTERN
+          (send
+            (const
+              (const nil? :ValisCommands) :ReindexDocument) :call
+              (hash $...))
+        PATTERN
+
+        def on_send(node)
+          if valis_reindex_document?(node)
+            add_offense(node, message: MSG)
+          end
+        end
+      end
+    end
+  end
+end
