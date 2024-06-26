116b415cc1562daa2b86e9bbdb73d02dfc581a39
adarshaks91 <148948906+adarshaks91@users.noreply.github.com>
2024-06-26 09:51:55 +0530

    Add PreventReindexFullESDocumentCop (#35)
    * Add PreventReindexFullESDocumentCop
    
    * Specs
    
    * Linting fix
    
    * Build fix

---
+++ spec/rubocop/cop/bugcrowd/prevent_reindex_full_es_document_cop_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Bugcrowd::PreventReindexFullESDocumentCop do
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offence when reindexing a full elasticsearch document' do
+    expect_offense(<<~RUBY)
+      ValisCommands::ReindexDocument.call(document_type: 'SubmissionDocument')
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Avoid reindexing the full Elasticsearch document. Consider reindexing only specific resource ids.
+    RUBY
+  end
+
+  it 'does not register an offense when reindexing only a specific resource ID' do
+    expect_no_offenses(<<~RUBY)
+      ValisReindexWorker.new.perform([submission.id], Submission.to_s)
+    RUBY
+  end
+end
