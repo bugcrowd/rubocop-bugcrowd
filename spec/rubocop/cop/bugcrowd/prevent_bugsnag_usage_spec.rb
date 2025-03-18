38a2f0138348dac96a130b7a4928b96ca224f125
Pramod Mahadev bhat <pramod.bhat@bugcrowd.com>
2025-03-18 12:36:44 +0530

    [BC-26568] Added rubocop warning for the use of Bugsnag

--- spec/rubocop/cop/bugcrowd/prevent_bugsnag_usage_spec.rb
+++ spec/rubocop/cop/bugcrowd/prevent_bugsnag_usage_spec.rb
@@ -2,20 +2,20 @@
 
 RSpec.describe RuboCop::Cop::Bugcrowd::PreventBugsnagUsage do
   subject(:cop) { described_class.new(config) }
-
   let(:config) { RuboCop::Config.new }
 
   it 'registers an offense when Bugsnag.notify is used' do
     expect_offense(<<~RUBY)
       Bugsnag.notify('Error')
-      ^^^^^^^^^^^^^^^^^^^^^^^ Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierService instead.
+      ^^^^^^^ Bugcrowd/PreventBugsnagUsage: Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierServiceinstead.
+      ^^^^^^^^^^^^^^^^^^^^^^^ Bugcrowd/PreventBugsnagUsage: Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierServiceinstead.
     RUBY
   end
 
   it 'registers an offense when Bugsnag is used as a constant' do
     expect_offense(<<~RUBY)
       Bugsnag.error('Error')
-      ^^^^^^^^^^^^^^^^^^^^^^^ Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierService instead.
+      ^^^^^^^ Bugcrowd/PreventBugsnagUsage: Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierServiceinstead.
     RUBY
   end
 
