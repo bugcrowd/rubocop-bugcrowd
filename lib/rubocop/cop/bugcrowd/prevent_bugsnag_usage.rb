3303775031786020ae68fc5d8159d72b89753758
Pramod Mahadev bhat <pramod.bhat@bugcrowd.com>
2025-03-20 16:37:53 +0530

    Updated rspec

--- lib/rubocop/cop/bugcrowd/prevent_bugsnag_usage.rb
+++ lib/rubocop/cop/bugcrowd/prevent_bugsnag_usage.rb
@@ -4,20 +4,12 @@ module RuboCop
   module Cop
     module Bugcrowd
       class PreventBugsnagUsage < RuboCop::Cop::Base
-        extend RuboCop::Cop::AutoCorrector
-
         MSG = 'Avoid using Bugsnag in the codebase. ' \
               'It has been replaced with ErrorNotifierService for error ' \
               'notification handling. Please use ErrorNotifierService instead.'
 
-        def_node_matcher :bugsnag_usage?, <<~PATTERN
-          (send (const nil? :Bugsnag) ...)
-        PATTERN
-
         def on_send(node)
-          return unless bugsnag_usage?(node)
-
-          add_offense(node, message: MSG)
+          add_offense(node, message: MSG) if node.receiver&.const_name == 'Bugsnag'
         end
       end
     end
--- spec/rubocop/cop/bugcrowd/prevent_bugsnag_usage_spec.rb
+++ spec/rubocop/cop/bugcrowd/prevent_bugsnag_usage_spec.rb
@@ -3,23 +3,48 @@
 RSpec.describe RuboCop::Cop::Bugcrowd::PreventBugsnagUsage, :config do
   subject(:cop) { described_class.new(config) }
 
-  it 'registers an offense when Bugsnag is used as a constant' do
-    expect_offense(<<~RUBY)
+  let(:message) do
+    'Avoid using Bugsnag in the codebase. ' \
+    'It has been replaced with ErrorNotifierService for error ' \
+    'notification handling. Please use ErrorNotifierService instead.'
+  end
+
+  it 'registers an offense when Bugsnag is used' do
+    expect_offense(<<~RUBY, message:)
       Bugsnag.error('Error')
-      ^^^^^^^^^^^^^^^^^^^^^^ Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierService instead.
+      ^^^^^^^^^^^^^^^^^^^^^^ %{message}
     RUBY
   end
 
   it 'registers an offense when Bugsnag.notify is used' do
-    expect_offense(<<~RUBY)
+    expect_offense(<<~RUBY, message:)
       Bugsnag.notify('Error')
-      ^^^^^^^^^^^^^^^^^^^^^^^ Avoid using Bugsnag in the codebase. It has been replaced with ErrorNotifierService for error notification handling. Please use ErrorNotifierService instead.
+      ^^^^^^^^^^^^^^^^^^^^^^^ %{message}
     RUBY
   end
 
-  it 'does not register an offense when ErrorNotifierService is used' do
+  it 'does not register an offense for ErrorNotifierService' do
     expect_no_offenses(<<~RUBY)
       ErrorNotifierService.notify('Error')
+    RUBY
+  end
+
+  it 'does not register an offense for unrelated constants' do
+    expect_no_offenses(<<~RUBY)
+      SomeOtherService.notify('Error')
+    RUBY
+  end
+
+  it 'does not register an offense for lowercase bugsnag' do
+    expect_no_offenses(<<~RUBY)
+      bugsnag.error('Error')
+    RUBY
+  end
+
+  it 'does not register an offense for local variable named Bugsnag' do
+    expect_no_offenses(<<~RUBY)
+      bugsnag = SomeService.new
+      bugsnag.error('Error')
     RUBY
   end
 end
